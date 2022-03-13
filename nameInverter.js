2c2593f6344f1a1732c54828bf607824a7e7ae51
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-03-13 15:58:42 -0400

    Refractor

--- nameInverter.js
+++ nameInverter.js
@@ -1,41 +1,35 @@
-const nameInverter = function(name) {
-const honorifics = ["Dr. ","Mrs. ","Mr. ","Ms "];
+const nameInverter = function (name) {
+  const honorifics = ["Dr.", "Mrs.", "Mr.", "Ms."];
+  // undefined and empty conditions
   if (name === '') {
     return '';
   }
-  if (name === '') {
-    return '';
+
+  if (name === undefined) {
+    throw 'Error'
   }
-  if (name === ' name ') {
-   let newName = name.trim();
-    return newName;
-  }
-  
-  if (name === 'first last') {
-  let newName = name.split(' ');
-  if (newName.length === 2){
-    newName.reverse();
-  }
-  return newName.join(",");
- 
-   }
-   if (honorifics.includes(name)){
-      return '';
+
+  // if name is at least one word
+  if (name.split(" ").length > 1) {
+    // trim white space 
+    const newName = name.split(" ").filter((x) => x);
+    if (newName.length === 1) {
+      let oneName = newName.join(" ");
+      // honorific conditions 
+      return honorifics.includes(oneName) ? "" : oneName;
+      //if honorifics includes name return honorific plus the name if it not return last, first
+    } else if (newName.length === 2) {
+     return honorifics.includes(newName[0]) ? `${newName[0]} ${newName[1]}` : `${newName[1]}, ${newName[0]}`;
+     
     }
-  if (honorifics.includes(name)){
-    return honorifics.name;
+      
+    // honorific plus last and first name condition
+    if (newName.length === 3) {
+      return `${newName[0]} ${newName[2]}, ${newName[1]}`;
+    }
   }
 
-
-
-   
-if (name === undefined) {
- throw 'Error'
-  }
-   
-    
-  
   return name;
-}
+};
 
 module.exports = nameInverter;
